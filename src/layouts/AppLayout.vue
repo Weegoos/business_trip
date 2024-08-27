90000392ae0151e0a7fd683780f72061c0592408
Weegoos <batyr.ashim05@mail.ru>
2024-08-27 10:49:40 +0500

    Business Trip Project: Quasar + VueJs

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <q-layout view="lHr LpR lFf" container style="height: 100vh">
+      <q-header reveal elevated bordered class="bg-white">
+        <q-toolbar class="bg-white text-black">
+          <q-btn flat round dense icon="menu" @click="drawer != drawer" />
+        </q-toolbar>
+        <q-drawer
+          side="left"
+          v-model="drawer"
+          bordered
+          :width="200"
+          :breakpoint="500"
+          content-class="bg-grey-3"
+        >
+        </q-drawer>
+      </q-header>
+      <q-page-container>
+        <q-page>
+          <router-view />
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(true);
+</script>
+
+<style scoped></style>
