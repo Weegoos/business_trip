945a82903f0c3d589e4531922eb7d9242bc9a5b2
Weegoos <batyr.ashim05@mail.ru>
2024-08-29 22:53:45 +0500

    Business Design: Setting up Styles

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,22 +1,13 @@
 <template>
   <div>
     <q-layout view="lHr LpR lFf" container style="height: 100vh">
-      <q-header reveal elevated bordered class="bg-white">
-        <q-toolbar class="bg-white text-black">
-          <q-btn flat round dense icon="menu" @click="drawer != drawer" />
+      <q-header reveal elevated bordered class="header">
+        <q-toolbar class="header text-white">
+          <q-btn flat label="OK" @click="onClick" />
         </q-toolbar>
-        <q-drawer
-          side="left"
-          v-model="drawer"
-          bordered
-          :width="200"
-          :breakpoint="500"
-          content-class="bg-grey-3"
-        >
-        </q-drawer>
       </q-header>
       <q-page-container>
-        <q-page>
+        <q-page class="body">
           <router-view />
         </q-page>
       </q-page-container>
