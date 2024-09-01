b3286e2d72be1ea1975410431093704dd3a02945
Weegoos <batyr.ashim05@mail.ru>
2024-09-01 17:34:01 +0500

    Business Trip: Design of the Explore Properties page has been completed

--- client/src/components/Footer/ExploreProperties.vue
+++ client/src/components/Footer/ExploreProperties.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="text-white">explore properties</div>
+  <div class="text-white container">
+    <p class="textHeadline">Start Your Real Estate Journey Today</p>
+    <section class="row q-gutter-md">
+      <div class="col-8">
+        <p class="textDescription">
+          Your dream property is just a click away. Whether you're looking for a
+          new home, a strategic investment, or expert real estate advice,
+          Estatein is here to assist you every step of the way. Take the first
+          step towards your real estate goals and explore our available
+          properties or get in touch with our team for personalized assistance.
+        </p>
+      </div>
+      <div class="col" align="center">
+        <q-btn
+          no-caps
+          class="button"
+          label="Explore Properties"
+          @click="onClick"
+        />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  background-color: #262626;
+  padding: 45px;
+}
+
+.button {
+  color: #ffffff;
+  background-color: #703bf7;
+  padding: 15px;
+}
+</style>
