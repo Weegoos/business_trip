b1bdbb821f660f47688b6ce7f45e98d76e9e8922
Weegoos <batyr.ashim05@mail.ru>
2024-09-01 17:35:56 +0500

    Business trip: Estatein Footer page has been created

---
+++ client/src/components/Footer/EstateinFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-white">Estatein</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- client/src/components/Footer/ExploreProperties.vue
+++ client/src/components/Footer/ExploreProperties.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="text-white container">
-    <p class="textHeadline">Start Your Real Estate Journey Today</p>
-    <section class="row q-gutter-md">
-      <div class="col-8">
-        <p class="textDescription">
-          Your dream property is just a click away. Whether you're looking for a
-          new home, a strategic investment, or expert real estate advice,
-          Estatein is here to assist you every step of the way. Take the first
-          step towards your real estate goals and explore our available
-          properties or get in touch with our team for personalized assistance.
-        </p>
-      </div>
-      <div class="col" align="center">
-        <q-btn
-          no-caps
-          class="button"
-          label="Explore Properties"
-          @click="onClick"
-        />
-      </div>
-    </section>
+    <div class="q-mt-lg q-ml-lg">
+      <p class="textHeadline">Start Your Real Estate Journey Today</p>
+      <section class="row q-gutter-md">
+        <div class="col-8">
+          <p class="textDescription">
+            Your dream property is just a click away. Whether you're looking for
+            a new home, a strategic investment, or expert real estate advice,
+            Estatein is here to assist you every step of the way. Take the first
+            step towards your real estate goals and explore our available
+            properties or get in touch with our team for personalized
+            assistance.
+          </p>
+        </div>
+        <div class="col" align="center">
+          <q-btn
+            no-caps
+            class="button"
+            label="Explore Properties"
+            @click="onClick"
+          />
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
