939cb166a202f5a30aeddfd37e615a7f5f763864
Weegoos <batyr.ashim05@mail.ru>
2024-09-01 17:00:32 +0500

    Business Trip: Added FAQ content

---
+++ client/src/components/MainPage/FAQPage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="text-white q-pa-xl">
+    <img
+      src="../../assets/indexPage/FeaturedPropertiesIntro.png"
+      alt=""
+      class="propertiesIcon"
+    />
+    <p class="textHeadline">Frequently Asked Questions</p>
+    <div class="row">
+      <section class="col">
+        <p class="textDescription">
+          Find answers to common questions about Estatein's services, property
+          listings, and the real estate process. We're here to provide clarity
+          and assist you every step of the way.
+        </p>
+      </section>
+      <q-btn
+        no-caps
+        class="button"
+        label="View All Testimonials"
+        @click="onClick"
+      />
+    </div>
+
+    <div class="q-my-lg row q-gutter-md">
+      <q-card class="my-card col" v-for="(items, id) in faqArray" :key="id">
+        <q-card-section>
+          <p class="sectionHeadline">{{ items.name }}</p>
+          <p class="sectionDescription">{{ items.description }}</p>
+        </q-card-section>
+        <q-card-actions align="left">
+          <q-btn no-caps class="button" label="Read more" @click="onClick" />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const faqArray = ref([
+  {
+    name: "How do I search for properties on Estatein?",
+    description:
+      "Learn how to use our user-friendly search tools to find properties that match your criteria.",
+  },
+  {
+    name: "What documents do I need to sell my property through Estatein?",
+    description:
+      "Find out about the necessary documentation for listing your property with us.",
+  },
+  {
+    name: "How can I contact an Estatein agent?",
+    description:
+      "Discover the different ways you can get in touch with our experienced agents.",
+  },
+]);
+</script>
+
+<style scoped>
+.button {
+  border: solid #262626 1px;
+  padding: 7px;
+}
+
+.my-card {
+  background-color: #1a1a1a;
+  padding: 10px;
+  border: solid #262626 1px;
+}
+
+.sectionHeadline {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.sectionDescription {
+  color: #999999;
+  font-size: 16px;
+}
+
+.button {
+  padding: 10px;
+  border: solid 1px #262626;
+}
+</style>
--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -10,6 +10,9 @@
       <div>
         <FeedBack />
       </div>
+      <div>
+        <FAQPage />
+      </div>
     </section>
   </div>
 </template>
@@ -18,6 +21,7 @@
 import DiscoveryPage from "../components/MainPage/DiscoveryPage.vue";
 import FeaturedProperties from "../components/MainPage/FeaturedProperties.vue";
 import FeedBack from "../components/MainPage/FeedBack.vue";
+import FAQPage from "../components/MainPage/FAQPage.vue";
 </script>
 
 <style></style>
