c699e0de9a7756a1599ea146a6a14903b3c3891d
Weegoos <batyr.ashim05@mail.ru>
2024-09-01 17:45:22 +0500

    Business Trip: The design of one part of the footer has been done

--- client/src/components/Footer/EstateinFooter.vue
+++ client/src/components/Footer/EstateinFooter.vue
@@ -1,9 +1,54 @@
 <template>
-  <div class="text-white">Estatein</div>
+  <div class="text-white">
+    <section class="row container">
+      <div class="col-4">
+        <section class="row q-gutter-sm">
+          <div class="q-mt-md">
+            <img icon="icon" src="../../assets/header/Symbol.png" alt="" />
+          </div>
+          <p class="textHeadline">Estatein</p>
+        </section>
+        <q-input
+          color="black"
+          bottom-slots
+          v-model="email"
+          label="Enter your email"
+          dark
+        >
+          <template v-slot:prepend>
+            <q-icon name="email" />
+          </template>
+          <template v-slot:append>
+            <q-icon
+              name="send"
+              style="transform: rotate(-40deg)"
+              @click="sendMessage"
+            />
+          </template>
+        </q-input>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { useQuasar } from "quasar";
+import { ref } from "vue";
+
+const email = ref("");
+const $q = useQuasar();
+const sendMessage = () => {
+  $q.notify({
+    message: "Clicked",
+  });
+};
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  padding: 50px;
+}
+.icon {
+  height: 10px;
+}
+</style>
