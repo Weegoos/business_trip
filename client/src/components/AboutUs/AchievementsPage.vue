05ce0e7ef4d5d778b9ebc2af8047e61f878394be
Weegoos <batyr.ashim05@mail.ru>
2024-09-10 09:55:15 +0500

    Business Trip: Achievements Page was been added

---
+++ client/src/components/AboutUs/AchievementsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img
+      class="propertiesIcon"
+      src="../../assets/indexPage/FeaturedPropertiesIntro.png"
+      alt=""
+    />
+    <p class="textHeadline">Our Achievements</p>
+    <p class="textDescription">
+      Our story is one of continuous growth and evolution. We started as a small
+      team with big dreams, determined to create a real estate platform that
+      transcended the ordinary.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -6,12 +6,16 @@
     <section>
       <ValuesPage />
     </section>
+    <section>
+      <AchievementsPage />
+    </section>
   </div>
 </template>
 
 <script setup>
 import JourneyPage from "../components/AboutUs/JourneyPage.vue";
 import ValuesPage from "../components/AboutUs/ValuesPage.vue";
+import AchievementsPage from "../components/AboutUs/AchievementsPage.vue";
 </script>
 
 <style></style>
