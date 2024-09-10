faa5b5d558682eeacdd3db6c1c4f05f7192978f0
Weegoos <batyr.ashim05@mail.ru>
2024-09-10 09:50:43 +0500

    Business Trip: Two components have been added to the values page

---
+++ client/src/components/AboutUs/ValuesPage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <section class="row q-gutter-md">
+      <div class="q-pa-xl col">
+        <img
+          class="propertiesIcon"
+          src="../../assets/indexPage/FeaturedPropertiesIntro.png"
+          alt=""
+        />
+        <p class="textHeadline">Our Values</p>
+        <p class="textDescription">
+          Our story is one of continuous growth and evolution. We started as a
+          small team with big dreams, determined to create a real estate
+          platform that transcended the ordinary.
+        </p>
+      </div>
+      <div class="col q-pa-xl">
+        <q-card class="card">
+          <q-card-section>
+            <div class="row q-gutter-md">
+              <div class="col">
+                <q-btn
+                  fab
+                  flat
+                  icon="code"
+                  style="border: #703bf7 1px solid; color: #a685fa"
+                  @click="onClick"
+                />
+                <span class="text-h6 text-bold q-ml-md">Trust</span>
+                <p class="text-body1 q-mt-sm">
+                  Trust is the cornerstone of every successful real estate
+                  transaction.
+                </p>
+              </div>
+
+              <div class="col">
+                <q-btn
+                  fab
+                  flat
+                  icon="code"
+                  style="border: #703bf7 1px solid; color: #a685fa"
+                  @click="onClick"
+                />
+                <span class="text-h6 text-bold q-ml-md">Excellence</span>
+                <p class="text-body1 q-mt-sm">
+                  We set the bar high for ourselves. From the properties we list
+                  to the services we provide.
+                </p>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card {
+  background-color: #141414;
+  border: solid #262626 8px;
+  border-radius: 7px;
+}
+</style>
--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -3,11 +3,15 @@
     <section>
       <JourneyPage />
     </section>
+    <section>
+      <ValuesPage />
+    </section>
   </div>
 </template>
 
 <script setup>
 import JourneyPage from "../components/AboutUs/JourneyPage.vue";
+import ValuesPage from "../components/AboutUs/ValuesPage.vue";
 </script>
 
 <style></style>
