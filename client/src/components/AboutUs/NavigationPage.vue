249a5d5e17775bf53cc8278b4c44c243bcb5bb9b
Weegoos <batyr.ashim05@mail.ru>
2024-09-11 16:21:38 +0500

    Business trip: navigation step has been integrated

--- client/src/components/AboutUs/NavigationPage.vue
+++ client/src/components/AboutUs/NavigationPage.vue
@@ -11,11 +11,37 @@
       purchase your dream property with ease. Here's a step-by-step guide to how
       it all works.
     </p>
+    <section class="q-mt-md row q-gutter-md">
+      <div v-for="(items, index) in 3" :key="index" class="col">
+        <q-card class="card col">
+          <q-card-section>
+            <div class="text-h6">Step {{ index + 1 }}</div>
+            <div>{{ items.name }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </section>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const navigationStep = ref([
+  {
+    name: "Discover a World of Possibilities",
+  },
+  {
+    name: "Narrowing Down Your Choices",
+  },
+]);
 </script>
 
-<style></style>
+<style scoped>
+.card {
+  background-color: #141414;
+  border: solid 1px;
+  border-color: #703bf7;
+  border-radius: 7px;
+}
+</style>
