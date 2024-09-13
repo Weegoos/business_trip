6a871b939fc59f1a128cb2f20a781cfcae4a431b
Weegoos <batyr.ashim05@mail.ru>
2024-09-13 12:07:21 +0500

    Business Trip: The content of Properties page has been added

--- client/src/pages/PropertiesPage.vue
+++ client/src/pages/PropertiesPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="text-white">Batyr</div>
+  <div class="text-white">
+    <section class="q-pa-xl">
+      <p class="textHeadline">Find Your Dream Property</p>
+      <p class="textDescription">
+        Welcome to Estatein, where your dream property awaits in every corner of
+        our beautiful world. Explore our curated selection of properties, each
+        offering a unique story and a chance to redefine your life. With
+        categories to suit every dreamer, your journey
+      </p>
+    </section>
+  </div>
 </template>
 
 <script>
