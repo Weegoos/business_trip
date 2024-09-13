7cfcb8a8fe3b690a2b8072bf9cc8c20dca3a0a7d
Weegoos <batyr.ashim05@mail.ru>
2024-09-13 10:42:19 +0500

    Business Trip: Content text has been added to the Valued Clients

--- client/src/components/AboutUs/ValuedClientsPage.vue
+++ client/src/components/AboutUs/ValuedClientsPage.vue
@@ -11,32 +11,47 @@
       clients across various industries. Here are some of the clients we've had
       the pleasure of serving
     </p>
-    <q-card class="card">
-      <q-card-section>
-        <div class="row q-gutter-md">
-          <section class="col">
-            <p style="color: #999999" class="text-body1">Since 2019</p>
-            <span class="text-bold text-h6">ABC Corporation</span>
-          </section>
-          <section class="col" align="right">
-            <q-btn
-              class="visitButton q-pa-md"
-              no-caps
-              label="Visit Website"
-              @click="onClick"
-            />
-          </section>
-        </div>
-      </q-card-section>
-      <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-      </q-card-section>
-    </q-card>
+    <div class="row q-gutter-md">
+      <q-card class="card col" v-for="(item, index) in client" :key="index">
+        <q-card-section>
+          <div class="row q-gutter-md">
+            <section class="col">
+              <p style="color: #999999" class="text-body1">
+                Since {{ item.year }}
+              </p>
+              <span class="text-bold text-h6">{{ item.coorporation }}</span>
+            </section>
+            <section class="col" align="right">
+              <q-btn
+                class="visitButton q-pa-md"
+                no-caps
+                label="Visit Website"
+                @click="onClick"
+              />
+            </section>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const client = ref([
+  {
+    coorporation: "ABC Corporation",
+    year: 2019,
+  },
+  {
+    coorporation: "GreenTech Enterprises",
+    year: 2018,
+  },
+]);
 </script>
 
 <style scoped>
