9f4d3e9e5e3d42b67014bce67b008ba30861366f
Weegoos <batyr.ashim05@mail.ru>
2024-09-13 10:36:00 +0500

    Business Trip: Content text has been added to the Valued Clients

--- client/src/components/AboutUs/ValuedClientsPage.vue
+++ client/src/components/AboutUs/ValuedClientsPage.vue
@@ -5,6 +5,12 @@
       src="../../assets/indexPage/FeaturedPropertiesIntro.png"
       alt=""
     />
+    <p class="textHeadline">Our Valued Clients</p>
+    <p class="textDescription">
+      At Estatein, we have had the privilege of working with a diverse range of
+      clients across various industries. Here are some of the clients we've had
+      the pleasure of serving
+    </p>
   </div>
 </template>
 
